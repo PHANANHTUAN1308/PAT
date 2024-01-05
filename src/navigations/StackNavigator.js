10c76818f4c7e7e1212d72be0239b61296a3dd34
Phan Anh Tuan <144339466+PHANANHTUAN1308@users.noreply.github.com>
2024-01-05 22:49:48 +0700

    up
    up

---
+++ src/navigations/StackNavigator.js
@@ -0,0 +1,46 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "../screen/HomeScreen";
+import DetailScreen from "../screen/DetailScreen";
+import ProductListScreen from "../screen/ProductListScreen";
+import CartScreen from "../screen/CartScreen";
+import OrderScreen from "../screen/OrderScreen";
+import ProfileScreen from "../screen/ProfileScreen";
+
+const Stack = createStackNavigator();
+
+const MainStackNavigator=() => {
+    return(
+        <Stack.Navigator>
+            <Stack.Screen name="home-screen" component={HomeScreen}></Stack.Screen>
+            <Stack.Screen name="detail-screen" component={DetailScreen}></Stack.Screen>
+            <Stack.Screen name="product-screen" component={ProductListScreen}></Stack.Screen>
+        </Stack.Navigator>
+    )
+}
+
+const CartStackNavigator = () =>{
+    return(
+        <Stack.Navigator>
+            <Stack.Screen name="cart-screen" component={CartScreen}></Stack.Screen>
+            
+        </Stack.Navigator>
+    )
+}
+
+const OrderStackNavigator = () =>{
+    return(
+        <Stack.Navigator>
+            <Stack.Screen name="order-screen" component={OrderScreen}></Stack.Screen>
+            
+        </Stack.Navigator>
+    )
+}
+
+const ProfileStackNavigator = () =>{
+    return(
+        <Stack.Navigator>
+            <Stack.Screen name="profile-screen" component={ProfileScreen}></Stack.Screen>
+            
+        </Stack.Navigator>
+    )
+}
